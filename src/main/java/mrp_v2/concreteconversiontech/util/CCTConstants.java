7e19ea692339b2b6acc8f3fee8f79cc693bffaf6
mrp-v2 <matthrewp@gmail.com>
2020-07-02 11:13:10 -0400

    created constants class

---
+++ src/main/java/mrp_v2/concreteconversiontech/util/CCTConstants.java
@@ -0,0 +1,7 @@
+package mrp_v2.concreteconversiontech.util;
+
+public class CCTConstants {
+	public static final String MODID = "concreteconversiontech";
+	public static final String TRANSLATION_STEM = "mrp_v2." + MODID + ".";
+	public static final String DISPLAY_NAME = "Concrete Conversion Tech";
+}
