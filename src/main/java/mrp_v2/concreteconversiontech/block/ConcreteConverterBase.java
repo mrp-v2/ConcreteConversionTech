c54b23863ee90e8b128abb1ed3a61990f05aaefe
mrp-v2 <matthrewp@gmail.com>
2020-07-02 11:13:54 -0400

    changed constructors

--- src/main/java/mrp_v2/concreteconversiontech/block/ConcreteConverterBase.java
+++ src/main/java/mrp_v2/concreteconversiontech/block/ConcreteConverterBase.java
@@ -1,8 +1,11 @@
 package mrp_v2.concreteconversiontech.block;
 
-import mrp_v2.concreteconversiontech.ConcreteConversionTech;
+import mrp_v2.concreteconversiontech.util.CCTConstants;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -10,14 +13,22 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.ToolType;
 
 abstract public class ConcreteConverterBase extends Block {
-	
+
 	public static final String ID_STEM = "concrete_converter_";
 
-	public ConcreteConverterBase(Properties properties, String blockID) {
-		super(properties);
-		this.setRegistryName(ConcreteConversionTech.MODID, blockID);
+	public ConcreteConverterBase(Material material, MaterialColor color, ToolType harvestTool, int harvestLevel,
+			float hardness, float resistance, SoundType sound, String blockID) {
+		super(Properties.create(material, color).harvestTool(harvestTool).harvestLevel(harvestLevel)
+				.hardnessAndResistance(hardness, resistance).sound(sound));
+		this.setRegistryName(CCTConstants.MODID, blockID);
+	}
+
+	public ConcreteConverterBase(Block base, String blockID) {
+		super(Properties.from(base));
+		this.setRegistryName(CCTConstants.MODID, blockID);
 	}
 
 	public static final ItemGroup CONCRETE_CONVERSION_TECH_ITEM_GROUP = new ItemGroup(2, "concreteConversionTech") {
