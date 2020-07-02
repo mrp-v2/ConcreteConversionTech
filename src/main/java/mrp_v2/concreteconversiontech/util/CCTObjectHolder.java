5e6b9706b1c00838190554fb60b7150c8d0ef910
mrp-v2 <matthrewp@gmail.com>
2020-07-02 15:06:55 -0400

    addec convert tier 1 item to object holder

--- src/main/java/mrp_v2/concreteconversiontech/util/CCTObjectHolder.java
+++ src/main/java/mrp_v2/concreteconversiontech/util/CCTObjectHolder.java
@@ -2,10 +2,14 @@ package mrp_v2.concreteconversiontech.util;
 
 import mrp_v2.concreteconversiontech.block.ConcreteConverterBase;
 import mrp_v2.concreteconversiontech.block.ConcreteConverterTier1;
+import net.minecraft.item.BlockItem;
 import net.minecraftforge.registries.ObjectHolder;
 
 public class CCTObjectHolder {
 
 	@ObjectHolder(CCTConstants.MODID + ":" + ConcreteConverterTier1.ID)
 	public static final ConcreteConverterBase CONCRETE_CONVERTER_TIER_1 = null;
+
+	@ObjectHolder(CCTConstants.MODID + ":" + ConcreteConverterTier1.ID)
+	public static final BlockItem CONCRETE_CONVERTER_TIER_1_ITEM = null;
 }
