cf18b819359a3e946aa739c5e812bf87a737af9f
mrp-v2 <matthrewp@gmail.com>
2020-07-02 17:02:56 -0400

    created client event handler

---
+++ src/main/java/mrp_v2/concreteconversiontech/client/eventhandler/ClientEventHandler.java
@@ -0,0 +1,19 @@
+package mrp_v2.concreteconversiontech.client.eventhandler;
+
+import mrp_v2.concreteconversiontech.util.CCTConstants;
+import mrp_v2.concreteconversiontech.util.CCTObjectHolder;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@EventBusSubscriber(modid = CCTConstants.MODID, value = Dist.CLIENT)
+public class ClientEventHandler {
+
+	@SubscribeEvent
+	public static void clientSetup(final FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(CCTObjectHolder.CONCRETE_CONVERTER_TIER_1, RenderType.getTranslucent());
+	}
+}
