fa39824a03d5606c9556511ae73d8d68699c9133
mrp-v2 <matthrewp@gmail.com>
2020-07-02 17:04:24 -0400

    change return type of item creation, update tab icon

--- src/main/java/mrp_v2/concreteconversiontech/block/ConcreteConverterBase.java
+++ src/main/java/mrp_v2/concreteconversiontech/block/ConcreteConverterBase.java
@@ -1,6 +1,7 @@
 package mrp_v2.concreteconversiontech.block;
 
 import mrp_v2.concreteconversiontech.util.CCTConstants;
+import mrp_v2.concreteconversiontech.util.CCTObjectHolder;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
@@ -10,7 +11,6 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.ToolType;
@@ -34,14 +34,16 @@ abstract public class ConcreteConverterBase extends Block {
 	public static final ItemGroup CONCRETE_CONVERSION_TECH_ITEM_GROUP = new ItemGroup(2, "concreteConversionTech") {
 		@OnlyIn(Dist.CLIENT)
 		public ItemStack createIcon() {
-			return new ItemStack(Items.REDSTONE);// TODO Update item reference
+			return new ItemStack(CCTObjectHolder.CONCRETE_CONVERTER_TIER_1);
 		}
 	};
 
-	public Item createBlockItem() {
+	public BlockItem createBlockItem() {
 		BlockState defaultState = this.getDefaultState();
-		return new BlockItem(this,
+		BlockItem bi = new BlockItem(this,
 				new Item.Properties().addToolType(this.getHarvestTool(defaultState), this.getHarvestLevel(defaultState))
-						.group(CONCRETE_CONVERSION_TECH_ITEM_GROUP)).setRegistryName(this.getRegistryName());
+						.group(CONCRETE_CONVERSION_TECH_ITEM_GROUP));
+		bi.setRegistryName(this.getRegistryName());
+		return bi;
 	}
 }
