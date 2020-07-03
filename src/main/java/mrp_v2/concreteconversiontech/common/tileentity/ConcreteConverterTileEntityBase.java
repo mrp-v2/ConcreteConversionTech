c7030f397748468796ce06b6d47ce5e5021dbf0c
mrp-v2 <matthrewp@gmail.com>
2020-07-03 10:57:33 -0400

    overrode read and write methods for NBT

--- src/main/java/mrp_v2/concreteconversiontech/common/tileentity/ConcreteConverterTileEntityBase.java
+++ src/main/java/mrp_v2/concreteconversiontech/common/tileentity/ConcreteConverterTileEntityBase.java
@@ -1,13 +1,27 @@
 package mrp_v2.concreteconversiontech.common.tileentity;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 
 abstract public class ConcreteConverterTileEntityBase extends TileEntity {
-	
+
 	protected static final String ID_STEM = "concrete_converter_tile_entity_";
 
 	public ConcreteConverterTileEntityBase(TileEntityType<?> tileEntityTypeIn) {
 		super(tileEntityTypeIn);
+	}
+
+	@Override
+	public void func_230337_a_(BlockState p_230337_1_, CompoundNBT p_230337_2_) {
+		// TODO Auto-generated method stub
+		super.func_230337_a_(p_230337_1_, p_230337_2_);
+	}
+
+	@Override
+	public CompoundNBT write(CompoundNBT compound) {
+		// TODO Auto-generated method stub
+		return super.write(compound);
 	}
 }
