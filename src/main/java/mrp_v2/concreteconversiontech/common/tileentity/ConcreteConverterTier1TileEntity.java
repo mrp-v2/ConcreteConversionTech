0040b073b2c9939cff12a9e000ac8da4189f6d8d
mrp-v2 <matthrewp@gmail.com>
2020-07-06 10:40:51 -0400

    implemented named container provider methods

--- src/main/java/mrp_v2/concreteconversiontech/common/tileentity/ConcreteConverterTier1TileEntity.java
+++ src/main/java/mrp_v2/concreteconversiontech/common/tileentity/ConcreteConverterTier1TileEntity.java
@@ -1,6 +1,11 @@
 package mrp_v2.concreteconversiontech.common.tileentity;
 
 import mrp_v2.concreteconversiontech.common.util.CCTObjectHolder;
+import mrp_v2.concreteconversiontech.common.util.MessageHelper;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.util.text.ITextComponent;
 
 public class ConcreteConverterTier1TileEntity extends AbstractConcreteConverterTileEntity {
 
@@ -8,5 +13,16 @@ public class ConcreteConverterTier1TileEntity extends AbstractConcreteConverterT
 
 	public ConcreteConverterTier1TileEntity() {
 		super(CCTObjectHolder.CONCRETE_CONVERTER_TIER_1_TILE_ENTITY_TYPE, 1, 16);
+	}
+
+	@Override
+	public ITextComponent getDisplayName() {
+		return MessageHelper.makeTranslation(ID, "displayName");
+	}
+
+	@Override
+	public Container createMenu(int p_createMenu_1_, PlayerInventory p_createMenu_2_, PlayerEntity p_createMenu_3_) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
