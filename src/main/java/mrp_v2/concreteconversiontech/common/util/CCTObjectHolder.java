1ec23e5e937a9d350816f7e4022918cf7bc9b78e
mrp-v2 <matthrewp@gmail.com>
2020-07-07 09:21:02 -0400

    set container type's registry name

--- src/main/java/mrp_v2/concreteconversiontech/common/util/CCTObjectHolder.java
+++ src/main/java/mrp_v2/concreteconversiontech/common/util/CCTObjectHolder.java
@@ -13,13 +13,16 @@ public class CCTObjectHolder {
 	public static final AbstractConcreteConverterBlock CONCRETE_CONVERTER_TIER_1_BLOCK;
 	public static final BlockItem CONCRETE_CONVERTER_TIER_1_BLOCK_ITEM;
 	public static final TileEntityType<?> CONCRETE_CONVERTER_TIER_1_TILE_ENTITY_TYPE;
-	public static final ContainerType<?> CONCRETE_CONVERTER_TIER_1_CONTAINER_TYPE;
+	public static final ContainerType<ConcreteConverterTier1Container> CONCRETE_CONVERTER_TIER_1_CONTAINER_TYPE;
 	static {
 		CONCRETE_CONVERTER_TIER_1_BLOCK = new ConcreteConverterTier1Block();
 		CONCRETE_CONVERTER_TIER_1_BLOCK_ITEM = CONCRETE_CONVERTER_TIER_1_BLOCK.createBlockItem();
 		CONCRETE_CONVERTER_TIER_1_TILE_ENTITY_TYPE = TileEntityType.Builder
 				.create(ConcreteConverterTier1TileEntity::new, CCTObjectHolder.CONCRETE_CONVERTER_TIER_1_BLOCK)
 				.build(null).setRegistryName(CCTConstants.MODID, ConcreteConverterTier1TileEntity.ID);
-		CONCRETE_CONVERTER_TIER_1_CONTAINER_TYPE = new ContainerType<>(ConcreteConverterTier1Container::new);
+		CONCRETE_CONVERTER_TIER_1_CONTAINER_TYPE = new ContainerType<ConcreteConverterTier1Container>(
+				ConcreteConverterTier1Container::new);
+		CONCRETE_CONVERTER_TIER_1_CONTAINER_TYPE.setRegistryName(CCTConstants.MODID,
+				ConcreteConverterTier1Container.ID);
 	}
 }
