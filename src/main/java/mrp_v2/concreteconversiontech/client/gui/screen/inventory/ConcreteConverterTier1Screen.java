65be5d62a7b36086a3d72e902a08adfde8ad8142
mrp-v2 <matthrewp@gmail.com>
2020-07-07 09:46:05 -0400

    created screen

---
+++ src/main/java/mrp_v2/concreteconversiontech/client/gui/screen/inventory/ConcreteConverterTier1Screen.java
@@ -0,0 +1,30 @@
+package mrp_v2.concreteconversiontech.client.gui.screen.inventory;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import mrp_v2.concreteconversiontech.common.inventory.container.ConcreteConverterTier1Container;
+import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+
+public class ConcreteConverterTier1Screen extends ContainerScreen<ConcreteConverterTier1Container> {
+
+	public ConcreteConverterTier1Screen(ConcreteConverterTier1Container screenContainer, PlayerInventory inv,
+			ITextComponent titleIn) {
+		super(screenContainer, inv, titleIn);
+		// TODO Auto-generated constructor stub
+	}
+
+	private static final ResourceLocation GUI_TEXTURES = new ResourceLocation("concreteconversiontech",
+			"textures/gui/container/concrete_converter_tier_1.png");
+
+	@Override
+	protected void func_230450_a_(MatrixStack p_230450_1_, float p_230450_2_, int p_230450_3_, int p_230450_4_) {
+		// TODO Auto-generated method stub
+		RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+		this.field_230706_i_.getTextureManager().bindTexture(GUI_TEXTURES);
+	}
+
+}
