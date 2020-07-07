63b0fcffb658de6c36f4909545790cf6ad9fef71
mrp-v2 <matthrewp@gmail.com>
2020-07-07 10:27:56 -0400

    fixed rendering

--- src/main/java/mrp_v2/concreteconversiontech/client/gui/screen/inventory/ConcreteConverterTier1Screen.java
+++ src/main/java/mrp_v2/concreteconversiontech/client/gui/screen/inventory/ConcreteConverterTier1Screen.java
@@ -14,14 +14,13 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 @OnlyIn(Dist.CLIENT)
 public class ConcreteConverterTier1Screen extends ContainerScreen<ConcreteConverterTier1Container> {
 
+	private static final ResourceLocation GUI_TEXTURES = new ResourceLocation("concreteconversiontech",
+			"textures/gui/container/concrete_converter_tier_1.png");
+
 	public ConcreteConverterTier1Screen(ConcreteConverterTier1Container screenContainer, PlayerInventory inv,
 			ITextComponent titleIn) {
 		super(screenContainer, inv, titleIn);
-		// TODO Auto-generated constructor stub
 	}
-
-	private static final ResourceLocation GUI_TEXTURES = new ResourceLocation("concreteconversiontech",
-			"textures/gui/container/concrete_converter_tier_1.png");
 
 	@Override
 	public void func_230430_a_(MatrixStack p_230430_1_, int p_230430_2_, int p_230430_3_, float p_230430_4_) {
@@ -36,6 +35,8 @@ public class ConcreteConverterTier1Screen extends ContainerScreen<ConcreteConver
 		// TODO Auto-generated method stub
 		RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
 		this.field_230706_i_.getTextureManager().bindTexture(GUI_TEXTURES);
+		int i = (this.field_230708_k_ - this.xSize) / 2;
+		int j = (this.field_230709_l_ - this.ySize) / 2;
+		this.func_238474_b_(p_230450_1_, i, j, 0, 0, this.xSize, this.ySize);
 	}
-
 }
