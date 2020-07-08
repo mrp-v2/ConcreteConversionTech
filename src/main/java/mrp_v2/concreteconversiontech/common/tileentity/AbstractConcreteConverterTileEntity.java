1432b8a90e38779ab9660da8922f2cb546a695f8
mrp-v2 <matthrewp@gmail.com>
2020-07-08 09:49:07 -0400

    changed abstract converter to blanket convert every 16 ticks

--- src/main/java/mrp_v2/concreteconversiontech/common/tileentity/AbstractConcreteConverterTileEntity.java
+++ src/main/java/mrp_v2/concreteconversiontech/common/tileentity/AbstractConcreteConverterTileEntity.java
@@ -1,6 +1,5 @@
 package mrp_v2.concreteconversiontech.common.tileentity;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,8 +21,6 @@ import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.Direction;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
@@ -34,94 +31,13 @@ import net.minecraftforge.items.ItemStackHandler;
 abstract public class AbstractConcreteConverterTileEntity extends TileEntity
 		implements ICapabilityProvider, ITickableTileEntity, INamedContainerProvider {
 
-	private class ConversionInfo {
-		private static final String ITEM_ID_NBT_ID = "ItemId";
-		private static final String CAN_CONVERT_NBT_ID = "CanConvert";
-		private static final String SOURCE_INDEX_NBT_ID = "SourceIndex";
-		private static final String DESTINATION_INDEX_NBT_ID = "DestinationIndex";
-
-		boolean canConvert;
-
-		int sourceIndex;
-
-		int destinationIndex;
-
-		Item itemConverting;
-
-		public ConversionInfo(boolean canConvert) {
-			this(canConvert, -1, -1, Items.AIR);
-		}
-
-		public ConversionInfo(boolean canConvert, int sourceIndex, int destinationIndex, Item itemConverting) {
-			this.canConvert = canConvert;
-			this.sourceIndex = sourceIndex;
-			this.destinationIndex = destinationIndex;
-			this.itemConverting = itemConverting;
-		}
-
-		@SuppressWarnings("deprecation")
-		public ConversionInfo(CompoundNBT nbt) {
-			canConvert = nbt.getBoolean(CAN_CONVERT_NBT_ID);
-			sourceIndex = nbt.getInt(SOURCE_INDEX_NBT_ID);
-			destinationIndex = nbt.getInt(DESTINATION_INDEX_NBT_ID);
-			itemConverting = Registry.ITEM.getOrDefault(new ResourceLocation(nbt.getString(ITEM_ID_NBT_ID)));
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj) {
-				return true;
-			}
-			if (obj == null) {
-				return false;
-			}
-			if (getClass() != obj.getClass()) {
-				return false;
-			}
-			ConversionInfo other = (ConversionInfo) obj;
-			if (canConvert != other.canConvert) {
-				return false;
-			}
-			if (destinationIndex != other.destinationIndex) {
-				return false;
-			}
-			if (sourceIndex != other.sourceIndex) {
-				return false;
-			}
-			if (itemConverting != other.itemConverting) {
-				return false;
-			}
-			return true;
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + (canConvert ? 1231 : 1237);
-			result = prime * result + destinationIndex;
-			result = prime * result + sourceIndex;
-			return result;
-		}
-
-		@SuppressWarnings("deprecation")
-		public CompoundNBT write() {
-			CompoundNBT nbt = new CompoundNBT();
-			nbt.putBoolean(CAN_CONVERT_NBT_ID, canConvert);
-			nbt.putInt(SOURCE_INDEX_NBT_ID, sourceIndex);
-			nbt.putInt(DESTINATION_INDEX_NBT_ID, destinationIndex);
-			ResourceLocation resourceLocation = Registry.ITEM.getKey(itemConverting);
-			nbt.putString(ITEM_ID_NBT_ID, resourceLocation == null ? "minecraft:air" : resourceLocation.toString());
-			return nbt;
-		}
-	}
-
 	protected static final String ID_STEM_PRE = "concrete_converter_";
 	protected static final String ID_STEM_POST = "_tile_entity";
 	private static final String INVENTORY_NBT_ID = "Inventory";
 	private static final String TICKS_SPENT_CONVERTING_NBT_ID = "TicksSpentConverting";
 
-	private static final String CURRENT_CONVERSION_NBT_ID = "ConversionInfo";
+	private static final int TICKS_PER_ITEM = 16;
+
 	protected static final Map<Item, Item> POWDER_TO_CONCRETE;
 
 	static {
@@ -152,78 +68,39 @@ abstract public class AbstractConcreteConverterTileEntity extends TileEntity
 	protected final ConcreteConverterItemStackHandler inventory;
 	private LazyOptional<ConcreteConverterItemStackHandler> inventoryLazyOptional;
 
-	private final float ticksPerItem;
-	private float ticksSpentConverting;
-
-	private ConversionInfo currentConversion;
+	private int ticksSpentConverting;
 
 	private String id;
 
-	public AbstractConcreteConverterTileEntity(TileEntityType<?> tileEntityTypeIn, int ioSlots, float ticksPerItem,
-			String id) {
+	public AbstractConcreteConverterTileEntity(TileEntityType<?> tileEntityTypeIn, int ioSlots, String id) {
 		super(tileEntityTypeIn);
-		this.ticksPerItem = ticksPerItem;
 		this.ticksSpentConverting = 0;
-		this.currentConversion = new ConversionInfo(false);
 		this.inventory = new ConcreteConverterItemStackHandler(ioSlots * 2, this);
 		this.inventoryLazyOptional = LazyOptional.of(() -> inventory);
 		this.id = id;
 	}
 
-	private ConversionInfo calculateConversionInfo() {
-		ArrayList<Integer> viableSourceSlots = new ArrayList<Integer>();
-		for (int i = inventory.getSlots() / 2 - 1; i >= 0; i--) {
-			if (isConcretePowder(inventory.getStackInSlot(i))) {
-				viableSourceSlots.add(i);
-			}
-		}
-		if (viableSourceSlots.size() <= 0) {
-			return new ConversionInfo(false);
-		}
-		for (int i1 : viableSourceSlots) {
-			for (int i2 = inventory.getSlots() / 2; i2 < inventory.getSlots(); i2++) {
-				if (inventory.getStackInSlot(i2).getCount() < inventory.getStackInSlot(i2).getMaxStackSize()) {
-					if (inventory.getStackInSlot(i2).getItem() == POWDER_TO_CONCRETE
-							.get(inventory.getStackInSlot(i1).getItem())) {
-						return new ConversionInfo(true, i1, i2, inventory.getStackInSlot(i1).getItem());
-					}
-				}
-			}
-			for (int i2 = inventory.getSlots() / 2; i2 < inventory.getSlots(); i2++) {
-				if (inventory.getStackInSlot(i2).isEmpty()) {
-					return new ConversionInfo(true, i1, i2, inventory.getStackInSlot(i1).getItem());
+	private int calculateDestinationSlot(int slot) {
+		Item powderItem = inventory.getStackInSlot(slot).getItem();
+		for (int i = inventory.getSlots() / 2; i < inventory.getSlots(); i++) {
+			if (inventory.getStackInSlot(i).getCount() < inventory.getStackInSlot(i).getMaxStackSize()) {
+				if (inventory.getStackInSlot(i).getItem() == POWDER_TO_CONCRETE.get(powderItem)) {
+					return i;
 				}
 			}
 		}
-		return new ConversionInfo(false);
-	}
-
-	public void contentsChanged() {
-		ConversionInfo conversionInfo = calculateConversionInfo();
-		if (currentConversion.canConvert && !conversionInfo.canConvert) {
-			ticksSpentConverting = 0;
-			currentConversion = conversionInfo;
-			this.markDirty();
-		} else if (!currentConversion.canConvert && conversionInfo.canConvert) {
-			currentConversion = conversionInfo;
-			this.markDirty();
-		} else if (currentConversion.canConvert && conversionInfo.canConvert) {
-			if (!currentConversion.equals(conversionInfo)) {
-				if (!isValid(currentConversion)) {
-					ticksSpentConverting = 0;
-					currentConversion = conversionInfo;
-					this.markDirty();
-				}
+		for (int i = inventory.getSlots() / 2; i < inventory.getSlots(); i++) {
+			if (inventory.getStackInSlot(i).isEmpty()) {
+				return i;
 			}
 		}
+		return -1;
 	}
 
 	private void convertItem(int sourceIndex, int destinationIndex) {
 		Item sourceItem = inventory.getStackInSlot(sourceIndex).getItem();
-		inventory.beginUpdate();
 		inventory.extractItem(sourceIndex, 1);
 		inventory.insertItem(destinationIndex, new ItemStack(POWDER_TO_CONCRETE.get(sourceItem), 1));
-		inventory.endUpdate();
 	}
 
 	@Override
@@ -233,8 +110,7 @@ abstract public class AbstractConcreteConverterTileEntity extends TileEntity
 	public void func_230337_a_(BlockState state, CompoundNBT nbt) {
 		super.func_230337_a_(state, nbt);
 		((ItemStackHandler) inventory).deserializeNBT(nbt.getCompound(INVENTORY_NBT_ID));
-		ticksSpentConverting = nbt.getFloat(TICKS_SPENT_CONVERTING_NBT_ID);
-		currentConversion = new ConversionInfo(nbt.getCompound(CURRENT_CONVERSION_NBT_ID));
+		ticksSpentConverting = nbt.getInt(TICKS_SPENT_CONVERTING_NBT_ID);
 	}
 
 	@Override
@@ -250,18 +126,6 @@ abstract public class AbstractConcreteConverterTileEntity extends TileEntity
 		return MessageHelper.makeTranslation(this.id, "display_name");
 	}
 
-	private boolean isValid(ConversionInfo conversionInfo) {
-		if (inventory.getStackInSlot(conversionInfo.sourceIndex).getItem() != conversionInfo.itemConverting) {
-			return false;
-		}
-		if (!inventory.getStackInSlot(conversionInfo.destinationIndex).isEmpty()
-				&& inventory.getStackInSlot(conversionInfo.destinationIndex).getItem() != POWDER_TO_CONCRETE
-						.get(inventory.getStackInSlot(conversionInfo.sourceIndex).getItem())) {
-			return false;
-		}
-		return true;
-	}
-
 	@Override
 	public void remove() {
 		super.remove();
@@ -270,13 +134,26 @@ abstract public class AbstractConcreteConverterTileEntity extends TileEntity
 
 	@Override
 	public void tick() {
-		if (currentConversion.canConvert) {
-			ticksSpentConverting++;
-			this.markDirty();
-			while (ticksSpentConverting >= ticksPerItem && currentConversion.canConvert) {
-				convertItem(currentConversion.sourceIndex, currentConversion.destinationIndex);
-				ticksSpentConverting -= ticksPerItem;
+		if (!inventory.isInputEmpty()) {
+			this.ticksSpentConverting++;
+			if (this.ticksSpentConverting >= TICKS_PER_ITEM) {
+				attemptConversions();
+			} else {
+				this.markDirty();
 			}
+		} else if (this.ticksSpentConverting != 0) {
+			this.ticksSpentConverting = 0;
+			this.markDirty();
+		}
+	}
+
+	private void attemptConversions() {
+		for (int i = 0; i < inventory.getSlots() / 2; i++) {
+			int destination = this.calculateDestinationSlot(i);
+			if (destination < 0) {
+				continue;
+			}
+			convertItem(i, destination);
 		}
 	}
 
@@ -284,8 +161,7 @@ abstract public class AbstractConcreteConverterTileEntity extends TileEntity
 	public CompoundNBT write(CompoundNBT compound) {
 		super.write(compound);
 		compound.put(INVENTORY_NBT_ID, ((ItemStackHandler) inventory).serializeNBT());
-		compound.putFloat(TICKS_SPENT_CONVERTING_NBT_ID, ticksSpentConverting);
-		compound.put(CURRENT_CONVERSION_NBT_ID, currentConversion.write());
+		compound.putInt(TICKS_SPENT_CONVERTING_NBT_ID, ticksSpentConverting);
 		return compound;
 	}
 }
