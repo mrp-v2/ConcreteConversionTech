e6f23b3f6b5a10dfb1cc42f5741c073da3197e75
mrp-v2 <matthrewp@gmail.com>
2020-08-25 20:19:27 -0400

    updated forge version

--- src/main/java/mrp_v2/concreteconversiontech/client/eventhandler/CCTClientRegistryHandler.java
+++ src/main/java/mrp_v2/concreteconversiontech/client/eventhandler/CCTClientRegistryHandler.java
@@ -1,18 +1,13 @@
 package mrp_v2.concreteconversiontech.client.eventhandler;
 
 import mrp_v2.concreteconversiontech.ConcreteConversionTech;
-import mrp_v2.concreteconversiontech.client.gui.screen.inventory.ConcreteConverterTier1Screen;
-import mrp_v2.concreteconversiontech.client.gui.screen.inventory.ConcreteConverterTier2Screen;
-import mrp_v2.concreteconversiontech.client.gui.screen.inventory.ConcreteConverterTier3Screen;
-import mrp_v2.concreteconversiontech.client.gui.screen.inventory.ConcreteConverterTier4Screen;
-import mrp_v2.concreteconversiontech.client.gui.screen.inventory.ConcreteConverterTier5Screen;
-import mrp_v2.concreteconversiontech.client.gui.screen.inventory.ConcreteConverterTier6Screen;
-import mrp_v2.concreteconversiontech.client.gui.screen.inventory.ConcreteConverterTier7Screen;
-import mrp_v2.concreteconversiontech.client.gui.screen.inventory.ConcreteConverterTier8Screen;
+import mrp_v2.concreteconversiontech.client.gui.screen.inventory.*;
 import mrp_v2.concreteconversiontech.common.util.CCTObjectHolder;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.gui.ScreenManager;
+import net.minecraft.item.BlockItem;
 import net.minecraft.world.biome.BiomeColors;
-import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.biome.BiomeRegistry;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ColorHandlerEvent;
@@ -21,47 +16,55 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @OnlyIn(Dist.CLIENT)
 @EventBusSubscriber(modid = ConcreteConversionTech.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-public class CCTClientRegistryHandler {
+public class CCTClientRegistryHandler
+{
+    static
+    {
+        ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_1_CONTAINER_TYPE,
+                ConcreteConverterTier1Screen::new);
+        ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_2_CONTAINER_TYPE,
+                ConcreteConverterTier2Screen::new);
+        ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_3_CONTAINER_TYPE,
+                ConcreteConverterTier3Screen::new);
+        ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_4_CONTAINER_TYPE,
+                ConcreteConverterTier4Screen::new);
+        ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_5_CONTAINER_TYPE,
+                ConcreteConverterTier5Screen::new);
+        ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_6_CONTAINER_TYPE,
+                ConcreteConverterTier6Screen::new);
+        ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_7_CONTAINER_TYPE,
+                ConcreteConverterTier7Screen::new);
+        ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_8_CONTAINER_TYPE,
+                ConcreteConverterTier8Screen::new);
+    }
 
-	@SubscribeEvent
-	public static void registerBlockColors(final ColorHandlerEvent.Block event) {
-		event.getBlockColors().register((state, reader, pos, tint) -> {
-			return reader != null && pos != null ? BiomeColors.getWaterColor(reader, pos) : -1;
-		}, CCTObjectHolder.CONCRETE_CONVERTER_TIER_1_BLOCK, CCTObjectHolder.CONCRETE_CONVERTER_TIER_2_BLOCK,
-				CCTObjectHolder.CONCRETE_CONVERTER_TIER_3_BLOCK, CCTObjectHolder.CONCRETE_CONVERTER_TIER_4_BLOCK,
-				CCTObjectHolder.CONCRETE_CONVERTER_TIER_5_BLOCK, CCTObjectHolder.CONCRETE_CONVERTER_TIER_6_BLOCK,
-				CCTObjectHolder.CONCRETE_CONVERTER_TIER_7_BLOCK, CCTObjectHolder.CONCRETE_CONVERTER_TIER_8_BLOCK);
-	}
+    @SubscribeEvent public static void registerBlockColors(final ColorHandlerEvent.Block event)
+    {
+        event.getBlockColors()
+                .register((state, reader, pos, tint) -> reader != null && pos != null ?
+                                BiomeColors.getWaterColor(reader, pos) :
+                                BiomeRegistry.field_244200_a.getWaterColor(), CCTObjectHolder.CONCRETE_CONVERTER_TIER_1_BLOCK,
+                        CCTObjectHolder.CONCRETE_CONVERTER_TIER_2_BLOCK,
+                        CCTObjectHolder.CONCRETE_CONVERTER_TIER_3_BLOCK,
+                        CCTObjectHolder.CONCRETE_CONVERTER_TIER_4_BLOCK,
+                        CCTObjectHolder.CONCRETE_CONVERTER_TIER_5_BLOCK,
+                        CCTObjectHolder.CONCRETE_CONVERTER_TIER_6_BLOCK,
+                        CCTObjectHolder.CONCRETE_CONVERTER_TIER_7_BLOCK,
+                        CCTObjectHolder.CONCRETE_CONVERTER_TIER_8_BLOCK);
+    }
 
-	@SubscribeEvent
-	public static void registerItemColors(final ColorHandlerEvent.Item event) {
-		event.getItemColors().register((itemStack, tint) -> {
-			return Biomes.PLAINS.getWaterColor();
-		}, CCTObjectHolder.CONCRETE_CONVERTER_TIER_1_BLOCK_ITEM, CCTObjectHolder.CONCRETE_CONVERTER_TIER_2_BLOCK_ITEM,
-				CCTObjectHolder.CONCRETE_CONVERTER_TIER_3_BLOCK_ITEM,
-				CCTObjectHolder.CONCRETE_CONVERTER_TIER_4_BLOCK_ITEM,
-				CCTObjectHolder.CONCRETE_CONVERTER_TIER_5_BLOCK_ITEM,
-				CCTObjectHolder.CONCRETE_CONVERTER_TIER_6_BLOCK_ITEM,
-				CCTObjectHolder.CONCRETE_CONVERTER_TIER_7_BLOCK_ITEM,
-				CCTObjectHolder.CONCRETE_CONVERTER_TIER_8_BLOCK_ITEM);
-	}
-
-	static {
-		ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_1_CONTAINER_TYPE,
-				ConcreteConverterTier1Screen::new);
-		ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_2_CONTAINER_TYPE,
-				ConcreteConverterTier2Screen::new);
-		ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_3_CONTAINER_TYPE,
-				ConcreteConverterTier3Screen::new);
-		ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_4_CONTAINER_TYPE,
-				ConcreteConverterTier4Screen::new);
-		ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_5_CONTAINER_TYPE,
-				ConcreteConverterTier5Screen::new);
-		ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_6_CONTAINER_TYPE,
-				ConcreteConverterTier6Screen::new);
-		ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_7_CONTAINER_TYPE,
-				ConcreteConverterTier7Screen::new);
-		ScreenManager.registerFactory(CCTObjectHolder.CONCRETE_CONVERTER_TIER_8_CONTAINER_TYPE,
-				ConcreteConverterTier8Screen::new);
-	}
+    @SubscribeEvent public static void registerItemColors(final ColorHandlerEvent.Item event)
+    {
+        event.getItemColors().register((itemStack, tint) ->
+                {
+                    BlockState blockstate = ((BlockItem) itemStack.getItem()).getBlock().getDefaultState();
+                    return event.getBlockColors().getColor(blockstate, null, null, tint);
+                }, CCTObjectHolder.CONCRETE_CONVERTER_TIER_1_BLOCK_ITEM, CCTObjectHolder.CONCRETE_CONVERTER_TIER_2_BLOCK_ITEM,
+                CCTObjectHolder.CONCRETE_CONVERTER_TIER_3_BLOCK_ITEM,
+                CCTObjectHolder.CONCRETE_CONVERTER_TIER_4_BLOCK_ITEM,
+                CCTObjectHolder.CONCRETE_CONVERTER_TIER_5_BLOCK_ITEM,
+                CCTObjectHolder.CONCRETE_CONVERTER_TIER_6_BLOCK_ITEM,
+                CCTObjectHolder.CONCRETE_CONVERTER_TIER_7_BLOCK_ITEM,
+                CCTObjectHolder.CONCRETE_CONVERTER_TIER_8_BLOCK_ITEM);
+    }
 }
--- src/main/java/mrp_v2/concreteconversiontech/client/gui/screen/inventory/AbstractConcreteConverterScreen.java
+++ src/main/java/mrp_v2/concreteconversiontech/client/gui/screen/inventory/AbstractConcreteConverterScreen.java
@@ -23,31 +23,31 @@ public abstract class AbstractConcreteConverterScreen<T extends AbstractConcrete
 		super(screenContainer, inv, titleIn);
 		this.guiTexture = new ResourceLocation(ConcreteConversionTech.MODID,
 				"textures/gui/container/concrete_converter_tier_" + tier + ".png");
-		this.field_230711_n_ = false;
+		this.passEvents = false;
 		this.ySize = ySize;
-		this.field_238745_s_ = this.ySize - 94;
+		this.playerInventoryTitleY = this.ySize - 94;
 	}
 
 	protected AbstractConcreteConverterScreen(T screenContainer, PlayerInventory inv, ITextComponent titleIn, int tier,
 			int ySize, int playerInventoryXShift) {
 		this(screenContainer, inv, titleIn, tier, ySize);
-		this.field_238744_r_ += playerInventoryXShift;
+		this.playerInventoryTitleX += playerInventoryXShift;
 	}
 
 	@Override
-	public void func_230430_a_(MatrixStack p_230430_1_, int p_230430_2_, int p_230430_3_, float p_230430_4_) {
-		this.func_230446_a_(p_230430_1_);
-		super.func_230430_a_(p_230430_1_, p_230430_2_, p_230430_3_, p_230430_4_);
+	public void render(MatrixStack p_230430_1_, int p_230430_2_, int p_230430_3_, float p_230430_4_) {
+		this.renderBackground(p_230430_1_);
+		super.render(p_230430_1_, p_230430_2_, p_230430_3_, p_230430_4_);
 		this.func_230459_a_(p_230430_1_, p_230430_2_, p_230430_3_);
 	}
 
 	@SuppressWarnings("deprecation")
 	@Override
-	protected void func_230450_a_(MatrixStack p_230450_1_, float p_230450_2_, int p_230450_3_, int p_230450_4_) {
+	protected void drawGuiContainerBackgroundLayer(MatrixStack p_230450_1_, float p_230450_2_, int p_230450_3_, int p_230450_4_) {
 		RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-		this.field_230706_i_.getTextureManager().bindTexture(guiTexture);
-		int i = (this.field_230708_k_ - this.xSize) / 2;
-		int j = (this.field_230709_l_ - this.ySize) / 2;
-		this.func_238474_b_(p_230450_1_, i, j, 0, 0, this.xSize, this.ySize);
+		this.minecraft.getTextureManager().bindTexture(guiTexture);
+		int i = (this.width - this.xSize) / 2;
+		int j = (this.height - this.ySize) / 2;
+		this.blit(p_230450_1_, i, j, 0, 0, this.xSize, this.ySize);
 	}
 }
